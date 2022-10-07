d134d0f53813607a7797e5a9cd34f4fb7d05a850
heyMP <mgp140@gmail.com>
2022-10-07 10:38:13 -0400

    feat: reorged dev deps.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,3 @@ import '@patternfly/pfe-select';
 import '@rhds/elements/rh-footer/rh-footer.js';
 import '@rhds/elements/rh-cta/rh-cta.js';
 import '@rhds/elements/rh-footer/rh-footer-lightdom.css';
-import reportWebVitals from './reportWebVitals';
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
